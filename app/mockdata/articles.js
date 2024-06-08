55cb755f1d6ff545966e5df8d9143e6a196a77cb
francescoguerrini <francescoguerrini1987@gmail.com>
2024-06-08 13:40:35 +0200

    data setup

---
+++ app/mockdata/articles.js
@@ -0,0 +1,178 @@
+// import { FaAlignJustify, FiSearch, FiArrowRight } from "react-icons/fa";
+
+const temi = [
+  "Ambiente",
+  "Economina",
+  "Mondo",
+  "Non Profit",
+  "Politica",
+  "Società",
+  "Welfare",
+];
+
+// const tags = []
+
+const articles = [
+  {
+    tag_text: "Migranti",
+    title: "Roccella Jonica, la Lampedusa che l'Italia ignora",
+    author_id: "1",
+    article_date: "29 Giugno 2022",
+  },
+  {
+    tag_text: "Allarmi",
+    title:
+      "Il collasso dei ghiacciai negli scatti del fotografo ambientale Fabiano Ventura",
+    author_id: "2",
+    article_date: "14 Luglio 2022",
+  },
+  {
+    tag_text: "Esperimenti",
+    title:
+      "Gli startupper? Li trovi al bistrot. Così il DumBo incuba relazioni",
+    author_id: "3",
+    article_date: "14 Luglio 2022",
+  },
+  {
+    tag_text: "Ucraina",
+    title: "Nelle città italiane tutti in piazza per e con Kiev",
+    author_id: "4",
+    article_date: "14 Luglio 2022",
+  },
+  {
+    tag_text: "Progetto Arca",
+    title: "“Qui Odessa”, fotografie, progetti e solidarietà",
+    author_id: "5",
+    article_date: "13 Luglio 2022",
+  },
+  {
+    tag_text: "Adolescenti",
+    title:
+      "Storie fra ragazzi permeate di violenza. A Napoli 12enne sfregiata da 17enne",
+    author_id: "6",
+    article_date: "13 Luglio 2022",
+  },
+  {
+    tag_text: "Economia Civile",
+    title:
+      "“Co-agitiamo”, Tiggiano una cooperativa di comunità contro lo spopolamento",
+    author_id: "7",
+    article_date: "12 Luglio 2022",
+  },
+  {
+    tag_text: "Sport",
+    title: "Troppi abusi nello sport minorile, il governo interviene",
+    author_id: "3",
+    article_date: "7 Luglio 2022",
+  },
+  {
+    tag_text: "Società",
+    title: "Donne e nuove generazioni: con loro riparte l’Italia",
+    author_id: "4",
+    article_date: "7 Luglio 2022",
+  },
+  {
+    tag_text: "Impresa Sociale",
+    title: "La Kasbah mazarese nella quale si gusta la cultura araba",
+    author_id: "8",
+    article_date: "3 Luglio 2022",
+  },
+  {
+    tag_text: "Sponsored",
+    title: "Sostenibilità sociale: accordo tra Dynamo Academy e Acsi",
+    author_id: "9",
+    article_date: "",
+  },
+  {
+    tag_text: "Persone",
+    title: "Uccisa ad Haiti suor Luisa, era l'angelo dei bambini",
+    author_id: "10",
+    article_date: "1 Luglio 2022",
+  },
+  {
+    tag_text: "Networking",
+    title: "Il fundraising in rotta verso Giardini-Naxos",
+    author_id: "5",
+    article_date: "1 Luglio 2022",
+  },
+  {
+    tag_text: "Società Civile",
+    title: "Lisa, il modello Croce Rossa per l'inclusione lavorativa",
+    author_id: "11",
+    article_date: "29 Giugno 2022",
+  },
+  {
+    tag_text: "Innovazione",
+    title: "Gli anziani del Sicomoro spopolano su TikTok",
+    author_id: "12",
+    article_date: "1 Luglio 2022",
+  },
+  {
+    tag_text: "Disabilità",
+    title: `"Emergenza sordi": quando un'app ti salva la vita`,
+    author_id: "13",
+    article_date: "1 Luglio 2022",
+  },
+  {
+    tag_text: "Traiettorie",
+    title: "Diego, il fruttivendolo che ha scelto la cooperazione sociale",
+    author_id: "14",
+    article_date: "1 Luglio 2022",
+  },
+  {
+    tag_text: "Sociale",
+    title: `Pedalare "Sicily Coast to Coast" per essere più inclusivi`,
+    author_id: "15",
+    article_date: "1 Luglio 2022",
+  },
+  {
+    tag_text: "Ucraina",
+    title: "Leopoli, andata + ritorno",
+    author_id: "16",
+    article_date: "14 Luglio 2022",
+  },
+];
+
+const authors = [
+  { id: 1, name: "Alessandro Puglia", image: "" },
+  { id: 2, name: "Ugo Lombi", image: "" },
+  { id: 3, name: "Diletta Grella", image: "" },
+  { id: 4, name: "Anna Spena", image: "" },
+  { id: 5, name: "Redazione", image: "" },
+  { id: 6, name: "Luca Cereda", image: "" },
+  { id: 7, name: "Emiliano Moccia", image: "" },
+  { id: 8, name: "Giovanni Gavardi", image: "" },
+  { id: 9, name: "Dynamo Academy", image: "" },
+  { id: 10, name: "Lucia Capuzzi", image: "" },
+  { id: 11, name: "Francesco Palazzo", image: "" },
+  { id: 12, name: "Luigi Alfonso", image: "" },
+  { id: 13, name: "Sara Bellingeri", image: "" },
+  { id: 14, name: "Giampaolo Cerri", image: "" },
+  { id: 15, name: "Giovanni Gamba", image: "" },
+  { id: 17, name: "Gilda Sciortino", image: "" },
+  { id: 18, name: "Paolo Dell’Oca", image: "" },
+  { id: 19, name: "Pasquale Pugliese", image: "" },
+  { id: 20, name: "Lorenzo Maria Alvaro", image: "" },
+  { id: 21, name: "Elena Zanella", image: "" },
+  { id: 22, name: "Laura Orestano", image: "" },
+];
+
+const blog = [
+  { author_id: "18", title: "Il Becco dell’oca" },
+  { author_id: "19", title: "Disarmato" },
+  { author_id: "20", title: "Battitiperminuto" },
+  { author_id: "21", title: "La Zanzarella" },
+  { author_id: "22", title: "Impact Q&A" },
+];
+
+const footerItems = [
+  "Il Gruppo",
+  "Note Legali",
+  "Chi Siamo",
+  "Condizioni d'uso",
+  "Privacy",
+  "Scrivici",
+  "Pubblicità",
+];
+
+export { articles, authors, temi, blog };
